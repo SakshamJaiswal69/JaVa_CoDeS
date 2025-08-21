9331f33867c49686413eb6ea6bb65aea0c18b388
SakshamJaiswal69 <a6136593@gmail.com>
2025-08-21 13:54:52 +0530

    java codes

---
+++ practice_set_2.java
@@ -0,0 +1,18 @@
+public class practice_set_2 {
+    public static void main(String[] args) {
+        // float a=7/4.0f*9/2.0f;
+        // System.out.print(a);
+
+
+        char grade = 'B';
+        // we are going to forcefullu add 8 to grade
+        grade = (char) (grade + 8);
+        System.out.println(grade);
+        // now we will subtract 8 from grade
+        grade = (char) (grade - 8);
+        System.out.println(grade);
+    
+    }
+}
+
+        
