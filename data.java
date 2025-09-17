88d7a58b737c555f9ccdf94decd31db617e9a3e1
SakshamJaiswal69 <a6136593@gmail.com>
2025-09-17 18:06:09 +0530

    upload day 2

---
+++ data.java
@@ -0,0 +1,23 @@
+// primitive data type
+/*byte =1 [-128 to 127]
+int -4
+long - 8
+float - 4
+double - 8
+char - 2
+boolean -1
+*/ 
+// Non-primitive data type
+/* String //String name =new String("ayush") // length()
+*/ 
+public class data {
+    public static void main(String[] args){
+        byte age = 30 ;
+        long phone = 497114797541L;
+        String name = "ayush jaiswal";
+        String friend =new String("ayush");
+        System.out.println(name.length());
+        System.out.println(friend);
+        
+    }
+}
