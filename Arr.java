bca068c24606545ec4417c94d1fb9bb331ce7cc3
SakshamJaiswal69 <a6136593@gmail.com>
2025-09-19 18:27:23 +0530

    upload day 3

---
+++ Arr.java
@@ -0,0 +1,20 @@
+// import java.util.Arrays;
+    public class Arr{
+        public static void main(String[]args){
+            // int [] age = new int [3];
+            // age [0] =20;
+            // age [1] =45;
+            // age [2] =46;
+            // System.out.println(age.length); // length here is a method of array
+            // System.out.println(age[1]); // length here is a method of array
+
+            
+            // int [] marks = {94, 65, 87};
+            // System.out.println(marks[1]);
+
+            int[][] final_marks={{89,75,61}, {86,79,81}};
+            System.out.println(final_marks[1][1]);
+        }
+
+       
+    }
---
+++ arthe.java
@@ -0,0 +1,34 @@
+public class arthe{
+    public static void main(String[]args){
+        // int a =46;
+        // int b = 8;
+        // System.out.println("sum of A and B : " + (a+b));
+
+        // int a =12;
+        // int b = 8;
+        // int c = a * b ; 
+        // System.out.println("sum of A and B : " + (c));
+
+        // int x =86;
+        // double y = 8;
+        // double z =x/y ;
+        // System.out.println("sum of A and B : " + (z));
+        
+        // int x =164;
+        // double y = 8.0;
+        // double z = x / y ;
+        // System.out.println("modulus of A and B : " + (z));
+
+        // increment operator 
+         
+         int a =1;
+        System.out.println(a);
+         a=++a;
+        System.out.println(a);
+         int b =1;
+        System.out.println(b);
+         b=--b;
+        System.out.println(b);
+
+    }
+}
